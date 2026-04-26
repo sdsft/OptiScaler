935d5191616e82a18788ba25d07ac2d85ceaa29c
cdozdil <cdozdil@gmail.com>
2026-04-26 21:14:08 +0300

    Fix depth transfer thread dispatch count for runtime compiled shader

--- OptiScaler/shaders/depth_transfer/DT_Common.h
+++ OptiScaler/shaders/depth_transfer/DT_Common.h
@@ -8,7 +8,7 @@ Texture2D<float> SourceTexture : register(t0);
 RWTexture2D<float> DestinationTexture : register(u0);
 
 // Shader to perform the conversion
-[numthreads(512, 1, 1)]
+[numthreads(16, 16, 1)]
 void CSMain(uint3 dispatchThreadID : SV_DispatchThreadID)
 {
     float srcColor = SourceTexture.Load(int3(dispatchThreadID.xy, 0));
